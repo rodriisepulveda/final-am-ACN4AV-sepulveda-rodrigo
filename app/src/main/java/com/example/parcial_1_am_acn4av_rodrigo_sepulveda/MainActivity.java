f31718d43b3a322d63bcb25ff73a2e7f47ab9e7b
Rodrigo Gabriel Sepulveda <rodrigo.sepulveda@davinci.edu.ar>
2024-09-24 21:32:09 -0300

    Agregados eventos, organizacion de variables, elemento dinamico, linearlayout

--- app/src/main/java/com/example/parcial_1_am_acn4av_rodrigo_sepulveda/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_rodrigo_sepulveda/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.parcial_1_am_acn4av_rodrigo_sepulveda;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Configurar el botón para navegar a ProductListActivity
         findViewById(R.id.buttonShowProducts).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ProductListActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // Evento para el botón "Agregar Producto"
+        findViewById(R.id.buttonAddProduct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_functionality_development, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Evento para el botón "Eliminar Producto"
+        findViewById(R.id.buttonDeleteProduct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toast_functionality_development, Toast.LENGTH_SHORT).show();
+
             }
         });
     }
--- app/src/main/java/com/example/parcial_1_am_acn4av_rodrigo_sepulveda/ProductListActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4av_rodrigo_sepulveda/ProductListActivity.java
@@ -1,15 +1,20 @@
 package com.example.parcial_1_am_acn4av_rodrigo_sepulveda;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductListActivity extends AppCompatActivity {
+    private List<Product> productList; // Lista de productos
+    private ProductAdapter productAdapter; // Adaptador para el RecyclerView
+    private EditText editTextProductName; // EditText para ingresar el nombre del producto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +24,37 @@ public class ProductListActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerViewProducts);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Crear una lista de productos
-        List<Product> productList = new ArrayList<>();
+        // Inicializar la lista de productos
+        productList = new ArrayList<>();
         productList.add(new Product("Producto 1 - Precio: 2$"));
-        productList.add(new Product("Product 2 - Precio: 4$"));
+        productList.add(new Product("Producto 2 - Precio: 4$"));
 
-        ProductAdapter adapter = new ProductAdapter(productList);
-        recyclerView.setAdapter(adapter);
+        productAdapter = new ProductAdapter(productList);
+        recyclerView.setAdapter(productAdapter);
+        editTextProductName = findViewById(R.id.editTextProductName);
 
-        // Configurar el botón para regresar a MainActivity
+        // boton para agregar productos
+        findViewById(R.id.buttonAddProduct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String productName = editTextProductName.getText().toString().trim();
+                if (!productName.isEmpty()) {
+                    productList.add(new Product(productName));
+                    productAdapter.notifyItemInserted(productList.size() - 1);
+                    editTextProductName.setText("");
+                    Toast.makeText(ProductListActivity.this, R.string.toast_product_added, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ProductListActivity.this, R.string.toast_product_add_error, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
         findViewById(R.id.buttonBackToMain).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish(); // Termina la actividad actual y regresa a MainActivity
+                finish();
             }
         });
     }
